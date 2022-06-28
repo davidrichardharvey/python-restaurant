11e40519d8401bad3bc085bfe1464a27b3557cd9
Mo <justonly4testing@gmail.com>
2022-06-28 19:44:38 +0100

    All tests are passing

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from restaurant import Table
 from unittest import main
 
 table01 = Table(5)
+print("get 2 risottos")
 table01.order("Risotto", 12.50, 2)
 print(table01.bill)
+print("get 3 burrito")
 table01.order("Burrito", 20.43, 3)
+print(table01.bill)
+print("REMOVE 2 burrito")
 table01.remove("Burrito", 20.43, 2)
 print(table01.bill)
+print("sub total")
 print(table01.get_subtotal())
+print("full bill")
 print(table01.get_total(0.15))
+print("bill split")
 print(table01.split_bill())
 
 # Run unit tests automatically
--- restaurant.py
+++ restaurant.py
@@ -1,2 +1,89 @@
 class Table:
-    pass
+
+    global bill
+
+    def __init__(self, people: int):
+        self.people = people
+        self.bill = []
+        self.sub_total = 0
+        self.total = 0
+
+
+    def order (self, food: str, price: float, quantity = 1):
+        self.food = food
+        self.price = price
+        self.quantity = quantity
+
+
+        self.bill.append({
+            'item': self.food,
+            'price': self.price,
+            'quantity': self.quantity})
+
+        # for entry in self.bill:
+        #     if entry['food'] == self.food:
+        #         print('same')
+        #         print(entry['food'])
+        #         entry['quantity'] += 1
+        #
+        #     else:
+        #         print("not same")
+        #         self.bill.append({
+        #             'item': self.food,
+        #             'price': self.price,
+        #             'quantity': self.quantity})
+
+
+    def remove(self, food, price, quantity):
+
+        for order in self.bill:
+            if order["item"] == food:
+                order["quantity"] -= quantity
+
+
+    def get_subtotal(self):
+
+        for order in self.bill:
+
+            self.sub_total += order["price"] * order["quantity"]
+
+        return round(self.sub_total,2)
+
+    def get_total(self, service_charge=0.10):
+
+        total_dic = {
+            'Sub Total':'',
+            'Service Charge': '',
+            'Total':''
+        }
+
+
+
+        subTotal = self.get_subtotal()
+        totalServiceCharge = round(subTotal * service_charge, 2)
+        total = subTotal + totalServiceCharge
+
+        total_dic['Sub Total'] = '£' + str(subTotal)+str(0)
+        total_dic['Service Charge'] = '£' + str(totalServiceCharge)
+        total_dic['Total'] = '£' + str(round(total, 2))
+        return total_dic
+
+        # self.service_charge = service_charge
+        # self.total = {
+        #
+        #     "Sub Total": int(self.get_subtotal()),
+        #     "Service Charge": round(self.service_charge * int(self.get_subtotal()),2),
+        #     # "Sub Total": int(self.sub_total),
+        #
+        #     "Total": (int(self.sub_total) * self.service_charge) + self.sub_total
+        # }
+        #
+        # return self.total
+
+
+
+    def split_bill(self):
+        return round( self.get_subtotal() / self.people, 2)
+
+
+
