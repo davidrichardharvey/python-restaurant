ca95b0ce25539e2fc194d7884acce2274158e1c9
moni3m <moniem.ibrahim@outlook.com>
2022-06-28 11:06:02 +0100

    moniems code

--- restaurant.py
+++ restaurant.py
@@ -9,40 +9,39 @@ class Table:
             if items["item"] == item and items["price"] == price:
                 items["quantity"] += quantity
                 break
-            else:
-                self.bill.append({"item": item, "price": price, "quantity": quantity})
+        else:
+            self.bill.append({"item": item, "price": price, "quantity": quantity})
 
-    def remove(self, item, price, quantity=0):
+    def remove(self, item, price, quantity=1):
         for items in self.bill:
             if items["item"] == item and items["price"] == price:
-                if items["quantity"] - quantity > 0:
-                    items["quantity"] -= quantity
+                if items["quantity"] - quantity == 0:
+                    self.bill.remove(bills)
+                elif items["quantity"] - quantity < 0:
+                    return False
                 else:
-                    self.bill.remove(item)
+                    items["quantity"] -= quantity
                 return True
-        return False
+        else:
+            return False
 
     def get_subtotal(self):
-        subtotal_cost = 0
-        for i in self.bill:
-            subtotal_cost += i["quantity"] * i["price"]
-        return subtotal_cost
+        sub_total = 0
+        for items in self.bill:
+            sub_total += items["price"] * items["quantity"]
+        self.sub_total = round(sub_total, 2)
+        return sub_total
 
     def get_total(self, service_charge=0.1):
-        total_dictionary = {
-            "Sub Total": "",
-            "Service Charge": "",
-            "Total": ""
-        }
+        self.get_subtotal()
+        total = self.sub_total + self.sub_total * service_charge
+        total_service_charge = round(self.sub_total * service_charge, 2)
 
-        sub_total = self.sub_total()
-        total_service_charge = round(sub_total * service_charge, 2)
-        total = sub_total + total_service_charge
+        return {"Sub Total": f"£{self.sub_total}0",
+                "Service Charge": f"£{total_service_charge}",
+                "Total": f"£{total}"}
 
-        total_dictionary["Sub Total"] = f"£{sub_total}"
-        total_dictionary["Service Charge"] = f"£{total_service_charge}"
-        total_dictionary["Total"] = f"£{round(total, 2)}"
-        return total_dictionary
-
-    def spilt_bill(self):
-        return round(self.sub_total/self.number_of_diners, 2)
+    def split_bill(self):
+        self.get_total()
+        split = self.sub_total / self.number_of_diners
+        return split
